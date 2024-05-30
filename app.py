9fdc474e2c9b9766a75d857c8a463f73625d5f2c
dolmerfresh <146840972+dolmerfresh@users.noreply.github.com>
2024-05-30 14:45:50 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,6 @@
+def introduction():
+    print ("This is a madlibs game, You'll be asked to input words and your words will help shape the story")
+    print("There's 3 storys to choose from.") 
+    print ("Depending on which location you'll pick youll get a different story")
+
+introduction()
