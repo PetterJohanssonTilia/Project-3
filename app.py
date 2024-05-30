0103c821bee56bdd863d4c31e9826a1908710706
dolmerfresh <146840972+dolmerfresh@users.noreply.github.com>
2024-05-30 15:24:10 +0200

    function: pickstory
    based on the user choice pick a story

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def introduction(): #The first message that appears when you run the program
     print ("This is a madlibs game, You'll be asked to input words and your words will help shape the story")
     print("There's 3 storys to choose from.") 
     print ("Depending on which location you'll pick youll get a different story")
-    choose_location() 
+
 
 def choose_location(): #Pick between 3 locations for 3 different stories
     print("Where do you want your story to take place?")
@@ -13,6 +13,7 @@ def choose_location(): #Pick between 3 locations for 3 different stories
     print("2. Desert")
     print("3. Ocean")
     choice = validate_input("Enter the number of the story you want to play: ", "number")
+    pickstory(choice)
     # Add code to handle the user's choice and start the selected story
     
 #Validates if its string or interger
@@ -26,6 +27,25 @@ def validate_input(prompt, input_type):
         else:
             print("Invalid input. Please enter a valid", input_type + ".")
 
+def pickstory(choice):
+    if choice == 1:
+        story_forest()
+    elif choice == 2:
+        story_desert()
+    elif choice == 3:
+        story_ocean()
+    else:
+        print("Invalid choice. Please select a number between 1 and 3.")    
+
+def story_forest():
+    print("Welcome to the Forest!")
+
+def story_desert():
+    print("Welcome to the Desert!")
+
+def story_ocean():
+    print("Welcome to the Ocean!")
 
 #Start
 introduction()
+choose_location() 
