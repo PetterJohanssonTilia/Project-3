d5ea2319167539709f46aab46f39332d9a281147
dolmerfresh <146840972+dolmerfresh@users.noreply.github.com>
2024-05-30 15:15:00 +0200

    function: validate_input
    checks if its letter or number

--- app.py
+++ app.py
@@ -12,10 +12,19 @@ def choose_location(): #Pick between 3 locations for 3 different stories
     print("1. Forest")
     print("2. Desert")
     print("3. Ocean")
-    choice = input("Enter the number of the story you want to play: ")
+    choice = validate_input("Enter the number of the story you want to play: ", "number")
     # Add code to handle the user's choice and start the selected story
-
-
+    
+#Validates if its string or interger
+def validate_input(prompt, input_type):
+    while True:
+        user_input = input(prompt)
+        if input_type == "number" and user_input.isdigit():
+            return int(user_input)
+        elif input_type == "letter" and user_input.isalpha():
+            return user_input
+        else:
+            print("Invalid input. Please enter a valid", input_type + ".")
 
 
 #Start
